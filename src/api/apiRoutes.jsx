cc4a3f3156c083e1b5aeea4b5c64aa3846d2f0f4
haileyng02 <haileynguyenss@gmail.com>
2022-12-20 13:19:40 +0700

    done login logout

--- src/api/apiRoutes.jsx
+++ src/api/apiRoutes.jsx
@@ -1,41 +1,56 @@
-export const getAccessTokenHeader = token => ({
-    headers: {
-      Authorization: 'Bearer ' + token
-    }
-  })
+export const getAccessTokenHeader = (token) => ({
+  headers: {
+    Authorization: "Bearer " + token,
+  },
+});
+
+//SIGN IN WITH EMAIL AND PASSWORD
+export const SIGN_IN = "/auth/signInWithEmailAndPassword";
+export const getSigninBody = (email, password) => ({
+  email: email,
+  password: password,
+});
 
 //GET ALL PRODUCTS
-export const GET_ALL_PRODUCTS = '/product-admin/getAllProducts'
+export const GET_ALL_PRODUCTS = "/product-admin/getAllProducts";
 
 //GET UNDELETED PRODUCTS
-export const GET_UNDELETED_PRODUCTS = '/product-admin/getUndeletedProducts'
+export const GET_UNDELETED_PRODUCTS = "/product-admin/getUndeletedProducts";
 
 //ADD PRODUCT
-export const ADD_PRODUCT = '/product-admin/addProducts'
-export const getAddProductBody = (productId, name, description, categoryId, price, sizeList, colorIdList) => ({
+export const ADD_PRODUCT = "/product-admin/addProducts";
+export const getAddProductBody = (
+  productId,
+  name,
+  description,
+  categoryId,
+  price,
+  sizeList,
+  colorIdList
+) => ({
   productId: productId,
   name: name,
   description: description,
   categoryId: categoryId,
   price: price,
   sizeList: sizeList,
-  colorIdList: colorIdList
-})
+  colorIdList: colorIdList,
+});
 
 //GET ALL STAFF
-export const GET_ALL_STAFF = '/staff/getAllStaff'
+export const GET_ALL_STAFF = "/staff/getAllStaff";
 
 //ADD STAFF
-export const ADD_STAFF = '/staff/addStaff'
+export const ADD_STAFF = "/staff/addStaff";
 export const getAddStaffBody = (email, role, startAt, status) => ({
   email: email,
   role: role,
   startAt: startAt,
-  status: status
-})
+  status: status,
+});
 
 //GET ALL USER
-export const GET_ALL_USER = '/user/getAllUser'
+export const GET_ALL_USER = "/user/getAllUser";
 
 //GET ALL ORDER
-export const GET_ALL_ORDER = '/staff-order/getAllOrder'
+export const GET_ALL_ORDER = "/staff-order/getAllOrder";
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Input, Form, Spin } from "antd";
+import appApi from "../api/appApi";
+import * as routes from "../api/apiRoutes";
+import signInProcess from "../utils/signInProcess";
+import decorationImage from "../images/login-decoration.png";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
 
-export default Login
+  //Sign in with email and password
+  const signInWithEmailAndPassword = async (email, password) => {
+    setLoading(true);
+    try {
+      const result = await appApi.post(
+        routes.SIGN_IN,
+        routes.getSigninBody(email, password)
+      );
+      console.log(result);
+      signInProcess({
+        token: result.data.access_token,
+        dispatch,
+        navigate,
+      });
+    } catch (err) {
+      if (err.response) {
+        const message = err.response.data.message;
+        if (message === "User's not exist") {
+          form.setFields([
+            {
+              name: "email",
+              errors: [message],
+            },
+          ]);
+        } else if (message === "Password incorrect") {
+          form.setFields([
+            {
+              name: "password",
+              errors: [message],
+            },
+          ]);
+        }
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
+        console.log(err.message);
+      }
+    }
+    setLoading(false);
+  };
+
+  const handleOnCLick = () => {
+    form.validateFields().then((values) => {
+      signInWithEmailAndPassword(values.email, values.password);
+    });
+  };
+
+  return (
+    <div className="bg-[#FFEFD3] w-screen h-screen px-[3.47%] py-[70px]">
+      <div className="bg-white w-full h-full rounded-[25px] border-1 border-[#00000080] flex justify-between">
+        <img src={decorationImage} alt="Login" />
+        <div className="h-full basis-1/2 flex flex-col justify-center  px-[3.7%]">
+          <Spin spinning={loading}>
+            <h1 className="font-inter font-bold text-[45px] text-customer-primary">
+              Welcome Back!
+            </h1>
+            <Form form={form}>
+              <Form.Item
+                name={"email"}
+                rules={[
+                  {
+                    required: true,
+                    message: "You must enter your email",
+                  },
+                  {
+                    type: "email",
+                    message: "This is not a valid email",
+                  },
+                ]}
+              >
+                <Input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  className="login-input"
+                />
+              </Form.Item>
+              <Form.Item
+                name={"password"}
+                rules={[
+                  {
+                    required: true,
+                    message: "You must enter password",
+                  },
+                ]}
+              >
+                <Input.Password
+                  type="password"
+                  placeholder="Password"
+                  className="login-input"
+                />
+              </Form.Item>
+            </Form>
+            <button
+              onClick={handleOnCLick}
+              className="bg-customer-primary w-full h-[52px] rounded-lg font-bold text-[#FAFAFA] mt-[53px]"
+            >
+              Sign In
+            </button>
+          </Spin>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
