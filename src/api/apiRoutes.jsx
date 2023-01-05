8bb9d2a723abceff968f47aae00cf8911b3ed936
chunkin02 <trungkieenn0202@gmail.com>
2023-01-05 13:26:01 +0700

    getUserMe(api-admin)

--- src/api/apiRoutes.jsx
+++ src/api/apiRoutes.jsx
@@ -355,3 +355,6 @@ export const getRevenuePerMonthBody = (year) => ({
 
 //EXPENDITURE OF MONTH
 export const EXPENDITURE_OF_MONTH = '/statistics/expenditureOfMonth'
+
+//GET USER ME
+export const USER_ME = '/user/me'
